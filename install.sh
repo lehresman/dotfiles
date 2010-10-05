983b446505539e9470856672ed6e50d48e141880
Luke Ehresman <luke@ehresman.org>
2010-10-05 06:37:39 -0400

    added installation script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+install () {
+	rm -rf $2
+	ln -sf $1 $2
+}
+
+confirm () {
+	SRCNAME="`pwd`/$1"
+	FNAME="$HOME/.$1"
+
+	echo "LINKING: $SRCNAME -> $FNAME"
+	if [ -e $FNAME ]; then
+		echo -n "  File $FNAME already exists.  Overwrite? [yN] "
+		read overwrite
+		if [ "$overwrite" = "y" ]; then
+			install $SRCNAME $FNAME
+		fi
+	else
+		install $SRCNAME $FNAME
+	fi
+}
+
+confirm "bash_profile"
+confirm "gitconfig"
+confirm "vimrc"
+confirm "vim"
