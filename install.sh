b08b38d0b62c67b55c6e299545023df86abc9b8b
Ross Dickey <rdickey@copperegg.com>
2014-09-05 08:53:07 -0500

    fix bug in install.sh that didn't do the rm before the symlink due to $HOME being double-added to the beginning of the path.

--- install.sh
+++ install.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 install () {
-	rm -rf $2
-	ln -sf $1 $2
+    rm -rf $2
+    ln -sf $1 $2
 }
 
 confirm () {
-	SRCNAME="`pwd`/$1"
-	FNAME="$HOME/.$1"
+    SRCNAME="`pwd`/$1"
+    FNAME="$HOME/.$1"
 
-	if [ -d "/proc" ]; then
-		OS="linux"
-	else
-		OS="mac"
-	fi
+    if [ -e "$SRCNAME.$OS" ]; then
+        SRCNAME="$SRCNAME.$OS"
+    fi
 
-	if [ -e "$SRCNAME.$OS" ]; then
-		SRCNAME="$SRCNAME.$OS"
-	fi
-
-	echo "LINKING: $SRCNAME -> $FNAME"
-	remove $FNAME
-	install $SRCNAME $FNAME
+    remove $FNAME
+    echo "LINKING: $SRCNAME -> $FNAME"
+    install $SRCNAME $FNAME
 }
 
 remove () {
-	if [ -h "$HOME/.$1" ]; then
-		echo "REMOVING: $HOME/.$1"
-		rm "$HOME/.$1"
-	fi
+    if [ -e "$1" ]; then
+        echo "MOVING: $1 to $HOME/.old-dotfiles/"
+        mkdir -p $HOME/.old-dotfiles
+        mv -f "$1" "$HOME/.old-dotfiles/" 2>/dev/null || rm -rf $1
+    fi
 }
+
+if [ -d "/proc" ]; then
+    OS="linux"
+else
+    OS="mac"
+fi
 
 # create symlinks to the dotfiles directory
 confirm "bash_profile"
-confirm "gitconfig"
+[[ $OS == "mac" ]] && confirm "gitconfig"
 confirm "vim"
 confirm "vimrc"
 confirm "gvimrc"
